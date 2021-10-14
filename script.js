99a029611d5cf6ce884fe8c4bc9c86ab0babc2ad
larslembke <lars46i6@stud.kea.dk>
2021-10-14 11:11:15 +0200

    what ever fucking lort

--- script.js
+++ script.js
@@ -5,9 +5,56 @@ const features = {
     spongebob: false,
     sirhiss: false
 }
+let colorValue;
+
+
+// const top_path = document.querySelector("#top_path");
+// const water_path = document.querySelector("#water_path");
+// const top_bubble_path = document.querySelector("#top_bubble_path");
+// const bottom_bubble_path = document.querySelector("#bottom_bubble_path");
+// const bottom_path = document.querySelector("#bottom_path");
+
+
+
+let currentElem = document.querySelector("#top_path");
 
 window.addEventListener("DOMContentLoaded", function() {
     console.log("DOM Loaded");
+
+    document.querySelectorAll("g").forEach((elem) => {
+        elem.addEventListener("click", (event) => {
+
+            console.log("color")
+
+            let lavaTarget = event.target;
+
+            console.log(lavaTarget)
+
+            colorValue = document.querySelector("#color_value").value;
+
+            console.log(colorValue)
+
+            lavaTarget.style.fill = colorValue;
+
+        })
+    });
+
+
+    function setColor(theColor) {
+
+        document.querySelector("#color_picker").addEventListener("input", function() {
+            theColor = document.querySelector("#color_value").value;
+
+            console.log(document.querySelector("#color_value").value)
+
+            return theColor;
+        });
+
+
+    }
+
+
+
 
     document.querySelectorAll(".option").forEach(option => option.addEventListener("click", toggleOption));
 
@@ -24,26 +71,29 @@ window.addEventListener("DOMContentLoaded", function() {
 
         let theFeature = createFeatureElement(feature);
 
-        document.querySelector("ul").appendChild(theFeature);
 
-        if (features[feature]) {
+
+        if (features[feature] === true) {
 
             console.log("yes?")
 
             target.classList.add("chosen");
 
+            document.querySelector("ul").appendChild(theFeature);
+
             document.querySelector(`[data-feature="${feature}"`).classList.remove("hide");
 
             theFeature.classList.add(feature);
 
-            const firstFrame = target.getBoundingClientRect();
+            const firstFrame = theFeature.getBoundingClientRect();
 
-            const lastFrame = theFeature.getBoundingClientRect();
+            const lastFrame = target.getBoundingClientRect();
 
-            console.log(firstFrame, lastFrame);
+            console.log("firstframe", firstFrame);
+            console.log("lastframe", lastFrame);
 
-            const deltaX = firstFrame.left - lastFrame.left;
-            const deltaY = firstFrame.top - lastFrame.top;
+            const deltaX = lastFrame.x - firstFrame.x;
+            const deltaY = lastFrame.y - firstFrame.y;
             const deltaW = firstFrame.width / lastFrame.width;
             const deltaH = firstFrame.height / lastFrame.height;
 
@@ -56,9 +106,9 @@ window.addEventListener("DOMContentLoaded", function() {
                     {
                         transformOrigin: "top left",
                         transform: "none"
-                    },
+                    }
                 ], {
-                    duration: 600,
+                    duration: 2000,
                     easing: "ease-in-out",
                 }
             );
@@ -68,24 +118,33 @@ window.addEventListener("DOMContentLoaded", function() {
 
             target.classList.remove("chosen");
             console.log(`Feature ${feature} is turned off!`);
+            document.querySelector(`[data-feature="${feature}"`).classList.add("hide");
+
+            theFeature.classList.remove(feature);
+
+            const theChild = document.querySelector(`.${feature}`);
+
+            document.querySelector(`ul [data-feature="${feature}"]`).remove(theFeature);
 
 
-            const firstFrame = target.getBoundingClientRect();
+            const firstFrame = theChild.getBoundingClientRect();
 
-            const lastFrame = theFeature.getBoundingClientRect();
+            const lastFrame = target.getBoundingClientRect();
 
-            console.log(firstFrame, lastFrame);
+
+            console.log("firstframe", firstFrame);
+            console.log("lastframe", lastFrame);
 
             const deltaX = firstFrame.left - lastFrame.left;
             const deltaY = firstFrame.top - lastFrame.top;
             const deltaW = firstFrame.width / lastFrame.width;
             const deltaH = firstFrame.height / lastFrame.height;
 
-            theFeature.animate(
+            theChild.animate(
                 [{
                         transformOrigin: "top left",
                         transform: `translateX(${deltaX}px)
-                translateY(${deltaY}px) scaleX(${deltaW}) scaleY(${deltaH})`,
+                translateY(${deltaY}px)`,
                     },
                     {
                         transformOrigin: "top left",
@@ -116,6 +175,5 @@ window.addEventListener("DOMContentLoaded", function() {
         return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
     }
 
-});
 
-//
+});
